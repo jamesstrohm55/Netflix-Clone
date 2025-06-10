d29dd42fb6a6c165f38481de628a191c7102a2a3
jamesstrohm55 <jamesstrohm98@gmail.com>
2025-06-10 12:06:38 -0300

    module clean up

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function signUp() {
 }
 
 const featuredMovies = [
-    { title: "Star Wars: Rouge One", image: "rouge1.jpg"  },
-    { title: "Tenant", image: "Tenent.jpg" },
-    { title: "Interstellar", image: "interstellar.jpg" },
-    { title: "Up", image: "up.jpg" },
+    { title: "Star Wars: Rouge One", image: './assets/rouge1.jpg'},
+    { title: "Tenant", image: './assets/Tenent.jpg' },
+    { title: "Interstellar", image: './assets/interstellar.jpg' },
+    { title: "Up", image: './assets/up.jpg' },
 ];
 
 function loadFeaturedMovies() {
@@ -25,20 +25,20 @@ function loadFeaturedMovies() {
 document.addEventListener('DOMContentLoaded', loadFeaturedMovies);
 
 const allMovies = [
-    { title: "Pirates of the Caribbean: On Strangers Tides", image: "pcst.jpg"  },
-    { title: "Avengers: Infinity War", image: "aiw.jpg" },
-    { title: "Pulp Fiction", image: "pf.jpg" },
-    { title: "The Hobbit: An Unexpected Journey", image: "hobbit.jpg" },
-    { title: "Dirty Dancing", image: "dd.webp" },
-    { title: "Black Panther", image: "bp.jpeg" },
-    { title: "Back to the Future Part II", image: "btf2.webp" },
-    { title: "Indiana Jones and the Last Crusade", image: "ijlc.jpg" },
-    { title: "Mad Max: Fury Road", image: "mmfr.jpg" },
-    { title: "Dark Knight", image: "dn.jpg" },
-    { title: "Tron: Legacy", image: "tron.jpg" },
-    { title: "Transformers: Revenge of the Fallen", image: "transformers.jpg" },
-    { title: "Iron Man 3", image: "ironman3.jpg" },
-    { title: "1917", image: "1917.jpg" },
+    { title: "Pirates of the Caribbean: On Strangers Tides", image: './assets/pcst.jpg' },
+    { title: "Avengers: Infinity War", image: './assets/aiw.jpg' },
+    { title: "Pulp Fiction", image: './assets/pf.jpg' },
+    { title: "The Hobbit: An Unexpected Journey", image: './assets/hobbit.jpg' },
+    { title: "Dirty Dancing", image: './assets/dd.webp' },
+    { title: "Black Panther", image: './assets/bp.jpeg' },
+    { title: "Back to the Future Part II", image: './assets/btf2.webp' },
+    { title: "Indiana Jones and the Last Crusade", image: './assets/ijlc.jpg' },
+    { title: "Mad Max: Fury Road", image: './assets/mmfr.jpg' },
+    { title: "Dark Knight", image: './assets/dn.jpg' },
+    { title: "Tron: Legacy", image: './assets/tron.jpg' },
+    { title: "Transformers: Revenge of the Fallen", image: './assets/transformers.jpg' },
+    { title: "Iron Man 3", image: './assets/ironman3.jpg' },
+    { title: "1917", image: './assets/1917.jpg' },
 ]
 
 function loadMovies() {
@@ -57,20 +57,20 @@ function loadMovies() {
 document.addEventListener('DOMContentLoaded', loadMovies);
 
 const allShows = [
-    { title: "From", image: "from.jpg"  },
-    { title: "The Mandalorian", image: "mando.jpg" },
-    { title: "Altered Carbon", image: "ac.jpg" },
-    { title: "Stranger Things", image: "st.jpg" },
-    { title: "Homeland", image: "hl.webp" },
-    { title: "Moonknight", image: "mk.webp" },
-    { title: "Yellowstone", image: "yellowstone.jpg" },
-    { title: "House of Cards", image: "hoc.jpg" },
-    { title: "Suits", image: "suits.jpg" },
-    { title: "House", image: "house.jpg" },
-    { title: "Game of Thrones", image: "got.jpg" },
-    { title: "Lost", image: "lost.jpg" },
-    { title: "Hannibal", image: "hannibal.jpg" },
-    { title: "Fallout", image: "fallout.webp" },
+    { title: "From", image: './assets/from.jpg' },
+    { title: "The Mandalorian", image: './assets/mando.jpg' },
+    { title: "Altered Carbon", image: './assets/ac.jpg' },
+    { title: "Stranger Things", image: './assets/st.jpg' },
+    { title: "Homeland", image: './assets/hl.webp' },
+    { title: "Moonknight", image: './assets/mk.webp' },
+    { title: "Yellowstone", image: './assets/yellowstone.jpg' },
+    { title: "House of Cards", image: './assets/hoc.jpg' },
+    { title: "Suits", image: './assets/suits.jpg' },
+    { title: "House", image: './assets/house.jpg' },
+    { title: "Game of Thrones", image: './assets/got.jpg' },
+    { title: "Lost", image: './assets/lost.jpg' },
+    { title: "Hannibal", image: './assets/hannibal.jpg' },
+    { title: "Fallout", image: './assets/fallout.webp' },
 ]
 
 function loadShows() {
